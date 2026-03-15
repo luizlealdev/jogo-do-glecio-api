b2aaab37f6e7a0bc63b568e73554eb49d746cf5c
luizlealdev <luizandreleal100@gmail.com>
2026-03-15 14:48:18 -0300

    Fix BigInt JSON serialization and clean up import formatting

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BigIntInterceptor } from './common/interceptors/bigint.interceptor';
-import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
-import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
 
+   //num entendi foi nada, mas resolveu meu erro. Isso é o bastante
+   (BigInt.prototype as any).toJSON = function () {
+      return this.toString();
+   };
+
    app.useGlobalInterceptors(new BigIntInterceptor());
    app.useGlobalInterceptors(new ResponseInterceptor());
-   
+
    app.useGlobalFilters(new HttpExceptionFilter());
 
    app.enableCors({
